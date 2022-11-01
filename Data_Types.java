a5bedd6339249d432e610783d31df70ce9c6dfc2
Dushyant Singh <dushyantsom60@gmail.com>
2022-11-01 22:21:25 +0530

    Added The Data_Types Example file

---
+++ Data_Types.java
@@ -0,0 +1,9 @@
+public class Data_Types{
+public static void main(String[] args) {
+    byte age = 30;
+    long viewsCount = 3_123_456_789L;
+    float price = 3.56F;
+    char letter = 'A';
+    boolean isEligible = false;
+   }
+}
