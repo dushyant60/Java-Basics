c93714902daabc94e542ba19e467f157da35d7c6
Dushyant Singh <dushyantsom60@gmail.com>
2022-11-02 23:02:59 +0530

    Added the code

--- Basic Programs/inputFromUser.java
+++ Basic Programs/inputFromUser.java
@@ -1,6 +1,13 @@
+import java.io.*;
+import java.util.Scanner;
 public class inputFromUser {
-    public static void main() {
-        
+    public static void main(String[] args) {
+        int num;
+        System.out.println("Enter the Integer:");
+        Scanner s = new Scanner(System.in);
+        num = s.nextInt();
+        System.out.println("The entered String is:" + num);
+
         
     }
 }
